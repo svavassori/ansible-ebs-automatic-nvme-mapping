048719f61298db392499a17c31f9bec71631addd
Sergio Vavassori <svavassori@conwet.com>
2019-09-27 11:41:45 +0200

    Makes device name match more explicit and working with and without '/dev/' prefix

--- files/ebs-nvme-mapping.sh
+++ files/ebs-nvme-mapping.sh
@@ -3,8 +3,8 @@
 PATH="${PATH}:/usr/sbin"
 
 for blkdev in $( nvme list | awk '/^\/dev/ { print $1 }' ) ; do
-  mapping=$(nvme id-ctrl --raw-binary "${blkdev}" | cut -c3073-3104 | tr -s ' ' | sed 's/ $//g')
+  mapping=$(nvme id-ctrl --vendor-specific "${blkdev}"| grep --perl-regexp --only-matching '(sd[b-z]|xvd[b-z])')
   if [[ "/dev/${mapping}" == /dev/* ]]; then
-    ( test -b "${blkdev}" && test -L "/dev/${mapping}" ) || ln -s "${blkdev}" "/dev/${mapping}"
+    ( test -b "${blkdev}" && test -L "/dev/${mapping}" ) || ln --symbolic "${blkdev}" "/dev/${mapping}"
   fi
 done
